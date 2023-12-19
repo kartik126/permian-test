77fd5477f943823311f1c4a020886ff78a8278dc
kartik126 <lavi9921@gmail.com>
2023-12-19 21:17:18 +0530

    style:icons shift to components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,8 @@ import useTyped from "@/hooks/useTyped";
 import imagesData from "../data/photos.json";
 import Loader from "@/components/Loader";
 import Typed from "typed.js";
+import NextIcon from "@/components/Icons/NextIcon";
+import BackIcon from "@/components/Icons/BackIcon";
 
 export default function Home() {
   const [title, settitlle] = useState("Compare Foods In India");
@@ -54,22 +56,7 @@ export default function Home() {
         <div>
           <ButtonBack className="absolute left-10 top-1/2 transform -translate-y-1/2 text-white">
             <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/10 dark:bg-gray-800/30 group-hover:bg-white/200 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-              <svg
-                className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 6 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M5 1 1 5l4 4"
-                />
-              </svg>
-              <span className="sr-only">Previous</span>
+              <BackIcon />
             </span>
           </ButtonBack>
           <Slider>
@@ -80,24 +67,7 @@ export default function Home() {
             ))}
           </Slider>
           <ButtonNext className="border-1 border-[#454545] absolute right-10 top-1/2 transform -translate-y-1/2 text-white">
-            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/10 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-              <svg
-                className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 6 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m1 9 4-4-4-4"
-                />
-              </svg>
-              <span className="sr-only">Next</span>
-            </span>
+            <NextIcon />
           </ButtonNext>
         </div>
       </CarouselProvider>
